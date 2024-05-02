0314e3580405941f7b8b1bb9f3b8df66d7f93e28
roshan thummar <roshanthummar43@gmail.com>
2024-05-02 12:03:37 +0530

    Initial commit

---
+++ src/Component/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import '../DescriptionBox/DescriptionBox.css';
+
+const DescriptionBox = () => {
+  return (
+    <div className='descriptionbox'>
+        <div className="descriptionbox-navigator">
+            <div className="descriptionbox-nav-box">Description</div>
+            <div className="descriptionbox-nav-box fade">Reviews (122)</div>
+        </div>
+        <div className="descriptionbox-description">
+            <p>An e-commerce website is an onlion platfrm that Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita, provident.
+                 Excepturi eligendi harum nisi. Nobis, hic, sit architecto atque accusamus, officiis provident et quas aliquam quaerat minus
+                  vero dignissimos dolore? Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum, praesentium fugit asperiores
+                   esse, soluta, vitae excepturi numquam dignissimos ut voluptates quae iste cupiditate quisquam officia architecto totam 
+                   officiis voluptatibus corporis! Lorem ipsum dolor sit, amet consectetur adipisicing elit. Temporibus dicta aperiam ducimus
+                    aliquid earum, mollitia veritatis ea neque non reprehenderit facere consectetur dolore porro animi tempore corporis 
+                    adipisci, ex veniam? and the global reach they offer. </p>
+                    <p>
+                       E-commerce websites dolor sit amet consectetur adipisicing elit. Accusantium et fugit veritatis reiciendis nostrum. Atque 
+                       facere expedita perferendis hic debitis asperiores natus blanditiis, quod commodi? Esse quam error possimus veniam? 
+                       Lorem ipsum, dolor sit amet consectetur adipisicing elit. Praesentium laudantium fugiat neque impedit non reprehenderit
+                        aut ipsa ipsam magni. Qui deserunt inventore velit voluptatem magnam numquam quasi with relevant information. 
+                    </p>
+        </div>
+    </div>
+  );
+}
+
+export default DescriptionBox;
